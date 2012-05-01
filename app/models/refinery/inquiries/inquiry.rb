a1457f71de14e958bd96e034a1bb695ba2bb8094
William Makley <wmakley@tortus.com>
2012-05-01 10:40:11 -0400

    Initial implementation of custom inquiry attributes
    All listed attributes become properties on the
    model. Supports default values.

--- app/models/refinery/inquiries/inquiry.rb
+++ app/models/refinery/inquiries/inquiry.rb
@@ -13,6 +13,8 @@ module Refinery
       validates :email, :format=> { :with =>  /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
 
       acts_as_indexed :fields => [:name, :email, :message, :phone]
+      
+      store :custom_store
 
       default_scope :order => 'created_at DESC'
 
@@ -20,6 +22,32 @@ module Refinery
 
       def self.latest(number = 7, include_spam = false)
         include_spam ? limit(number) : ham.limit(number)
+      end
+      
+      def self.custom_attributes
+        Inquiries.custom_inquiry_attributes
+      end
+      def custom_attributes
+        self.class.custom_attributes
+      end
+      
+      custom_attributes.each do |attribute, config|
+        logger.debug "Custom Inquiry attribute: #{attribute}"
+        
+        attribute_name = attribute.to_sym     
+        
+        define_method(attribute_name) { custom_store[attribute_name] }
+        define_method("#{attribute_name}=") { |val| custom_store[attribute_name] = val }
+        attr_accessible(attribute)
+      end
+      
+      if (attrs_with_defaults = custom_attributes.reject {|key, config| config[:default].nil? })
+        define_method(:init_custom_attributes) do
+          attrs_with_defaults.each do |attribute, config|
+            self.send("#{attribute}=", config[:default])
+          end
+        end
+        after_initialize :init_custom_attributes
       end
 
     end
--- lib/refinery/inquiries/configuration.rb
+++ lib/refinery/inquiries/configuration.rb
@@ -5,5 +5,8 @@ module Refinery
     config_accessor :show_contact_privacy_link
 
     self.show_contact_privacy_link = true
+    
+    config_accessor :custom_inquiry_attributes
+    self.custom_inquiry_attributes = {}
   end
 end
