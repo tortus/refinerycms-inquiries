4c89de1c1847b8eaa766731859773785656eefe1
William Makley <wmakley@tortus.com>
2013-11-05 16:45:10 -0500

    make config accessor declaration style match upstream

--- lib/refinery/inquiries/configuration.rb
+++ lib/refinery/inquiries/configuration.rb
@@ -3,13 +3,11 @@ module Refinery
     include ActiveSupport::Configurable
 
     config_accessor :show_contact_privacy_link
+    config_accessor :custom_inquiry_attributes
     config_accessor :send_notifications_for_inquiries_marked_as_spam
 
     self.show_contact_privacy_link = true
-
-    config_accessor :custom_inquiry_attributes
     self.custom_inquiry_attributes = {}
-
     self.send_notifications_for_inquiries_marked_as_spam = false
   end
 end
